0c30fcc1bec595c6bd634ec97187c0aeecf959d4
DimitriNakos <dimi1nakos@gmail.com>
2020-11-23 17:39:28 -0500

    added barebones files and pseudocode to index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+
+
+
+//  create a command line application that accepts input
+
+
+
+//  create a prompt for the Title
+// generate a title from the input
+
+
+//  create a prompt for installation instructions
+// generate a title from the installation instructions
+
+//  create a prompt for usage instructions
+// generate a title from the usage instructions
+
+//  create a prompt for contribution guidelines
+// generate a title from the contribution guidelines
+
+//  create a prompt for test instructions
+// generate a title from the test instructions
+
+//  when I enter my github username it is added to the section of the readme entitled Questions
+//  a link is added to my github profile
+
+//  create a prompt for my email address
+//  add my email address to the section of the reame entitled Questions
+// instructions are added with how to reach me with additional questions
+
+
+
+//  Give a list of options to choose a license.
+//  when the license is selected it is added to the top of the readme
+//  a notice is added to the section of the readme entitaled license
+//  explain which license the application is covered under
+
+
+
+//  When the user clicks on links in the Table of contents they are taken to corresponding part.  
