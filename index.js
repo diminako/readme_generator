a138562a3ac8208d5ad4ec957b5a1679be16b263
DimitriNakos <dimi1nakos@gmail.com>
2020-11-24 23:04:20 -0500

    added some questions and filling in various functions

--- index.js
+++ index.js
@@ -1,25 +1,78 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is the application title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please enter a description or your application?',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the steps to install your project?',
+        name: 'instructions'
+    },
+    {
+        type: 'input',
+        message: 'What are examples of use for your application?',
+        name: 'usage'
+    },
+    {
+        type: 'confirm',
+        message: 'Were there any collaborators/contributions to your application?',
+        name: 'collabs'
+    },
+    {
+        type: 'input',
+        message: 'Please describe how to test your application.',
+        name: 'tests',
+    },
+    {
+        type: 'checkbox',
+        message: 'Which license(s) would you like to include for your application readme?',
+        name: 'license',
+        choices: ["HTML", "CSS", "JavaScript", "JQuery", "Node.js", "GIT"]
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((data) => {
+        writeToFile
+    });
 }
 
 // function call to initialize program
 init();
 
-
-
-
 //  create a command line application that accepts input
-
 
 
 //  create a prompt for the Title
